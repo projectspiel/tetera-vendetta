0edd5d3badd79bc3e22558b2cd073838dbd4eb4f
Eduardo Ojeda <eduardo.ojeda@researchgate.net>
2015-01-21 00:17:40 +0100

    2D sprites in 3D space, textured cubes

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour {
+
+	void Awake () {
+		transform.rotation = Camera.main.transform.rotation;
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,20 +2,16 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float speed = 6f;            // The speed that the player will move at.
+	public float speed = 6f;
 	
-	Vector3 movement;                   // The vector to store the direction of the player's movement.
-	Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-	int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
-	float camRayLength = 100f;          // The length of the ray from the camera into the scene.
+	Vector3 movement;
+	int floorMask;
+	float camRayLength = 100f;
+	float smoothing = 5f;
 	
 	void Awake ()
 	{
-		Debug.Log ("Awake");
-		// Create a layer mask for the floor layer.
 		floorMask = LayerMask.GetMask ("Floor");
-
-		playerRigidbody = GetComponent <Rigidbody> ();
 	}
 	
 	
@@ -23,15 +19,9 @@ public class PlayerMovement : MonoBehaviour
 	{
 		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit floorHit;
-		float smoothing = 5f;
 		
 		if (Input.GetAxis ("Fire1") == 1 && Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
-			Debug.Log ("Hit");
-//			Vector3 playerToMouse = floorHit.point - transform.position;			
-//			playerToMouse.y = 0f;
 			transform.position = Vector3.Lerp (transform.position, floorHit.point, smoothing * Time.deltaTime);
-//			movement = movement.normalized * speed * Time.deltaTime;
-//			playerRigidbody.MovePosition (transform.position + movement);
 		}
 			
 	}
